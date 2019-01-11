8a6615c110ed241becce3efcda9db84e369a0e94
François Deschamps <francois@fdweb.fr>
2019-01-11 13:34:40 +0100

    bug

--- project/src/Controller/DefaultController.php
+++ project/src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
